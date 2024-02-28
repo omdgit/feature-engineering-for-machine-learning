139d9cb6a5a3298cb85bd49d33d221ca9de59cb4
omdgit <omdgit@users.noreply.github.com>
2024-02-28 06:41:03 -0500

    updates to discretizing variables

--- omd/utils.py
+++ omd/utils.py
@@ -1,4 +1,33 @@
 from optbinning import OptimalBinning
+from sklearn.base import TransformerMixin, BaseEstimator
+
+# class CustomDiscretizer(TransformerMixin, BaseEstimator):
+#     def __init__(self, variables=None, dtype='numerical', max_n_bins=10, min_bin_size=0.05):
+#         self.variables = variables
+#         self.max_n_bins = max_n_bins
+#         self.min_bin_size = min_bin_size
+#         self.optbinners = {}
+#         for variable in self.variables:
+#             self.optbinners[variable] = OptimalBinning(name=variable, 
+#                                                        dtype=dtype, 
+#                                                        solver="cp", 
+#                                                        max_n_bins=self.max_n_bins, 
+#                                                        min_bin_size=self.min_bin_size)
+
+#     def fit(self, X, y=None):
+#         for variable in self.variables:
+#             self.optbinners[variable].fit(X[variable], y)
+#         return self
+
+#     def transform(self, X):
+#         for variable in self.variables:
+#             X[variable] = self.optbinners[variable].transform(X[variable])
+#         return X.drop(columns=self.variables)
+
+#     def fit_transform(self, X, y=None):
+#         return self.fit(X, y).transform(X)
+
+
 
 class CustomDiscretizer:
     def __init__(self, variable, dtype='numerical', max_n_bins=10, min_bin_size=0.05):
